57606c1907dfefcf59315b562bf3dbce98629a18
luzG25 <gdluz@uta.cv>
2025-07-05 11:36:13 -0100

    aprimoracao de pagina incial

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -13,27 +13,26 @@ export default async function Home() {
   const produtosMaisVendidos = (await fetchProdutosMaisVendidos()) as Produto[];
 
   return (
-    <div>
+    <div className=" ">
       <BannerCarousel />
-      <div className="flex flex-col gap-10 px-4 md:px-44 mt-4 mb-5">
+      <div className="flex flex-col gap-5 px-4 md:px-44 ">
         <SeccaoMarker>Categorias Populares</SeccaoMarker>
+        <CategoriaNavigationServer />
       </div>
-      <CategoriaNavigationServer />
-      {/* Produtos em Destaque */}
-      <div className="flex flex-col gap-10 px-4 md:px-44 mt-4 mb-10">
-        <SeccaoMarker href="/destaques">Produtos em Destaque</SeccaoMarker>
-        <ProdutoListaLg>
-          {produtosMaisVendidos.map((prod) => (
-            <ProdutoCard key={prod.id} produto={prod} />
-          ))}
-        </ProdutoListaLg>
-      </div>
-      <div className="flex flex-col gap-10 mb-30 mt-4">
+
+      <div className="flex flex-col gap-10 mb-30 mt-10">
+        {/* Produtos em Destaque */}
+        <div className="flex flex-col gap-10 px-4 md:px-44  ">
+          <SeccaoMarker href="/destaques">Produtos em Destaque</SeccaoMarker>
+          <ProdutoListaLg>
+            {produtosMaisVendidos.map((prod) => (
+              <ProdutoCard key={prod.id} produto={prod} />
+            ))}
+          </ProdutoListaLg>
+        </div>
+
         {/*Produtos TOP*/}
-        <div className="flex flex-col gap-10 px-4 md:px-44 mt-4 mb-10">
-          {/*<h1 className="inline bg-orange-500 text-gray-800 p-2 text-2xl font-bold pl-10 pr-5 left-2 rounded-r-lg">
-          Produtos TOP
-        </h1>*/}
+        <div className="flex flex-col gap-10 px-4 md:px-44 ">
           <SeccaoMarker href="/top">Produtos TOP</SeccaoMarker>
           <ProdutoListaLg>
             {produtosMaisVendidos.map((prod) => (
@@ -43,12 +42,12 @@ export default async function Home() {
         </div>
 
         {/*Produtos em Promoção */}
-        <div className="flex flex-col gap-10 px-4 md:px-44  mb-10">
+        <div className="flex flex-col gap-10 px-4 md:px-44 ">
           <Promocoes />
         </div>
 
         {/*Produtos Mais Vendidos*/}
-        <div className="flex flex-col gap-10 px-4 md:px-44 mt-4 mb-10">
+        <div className="flex flex-col gap-10 px-4 md:px-44  ">
           {/*<h1 className="inline bg-orange-500 text-gray-800 p-2 text-2xl font-bold pl-10 pr-5 left-2 rounded-r-lg">
           Produtos Mais Vendidos
         </h1>*/}
