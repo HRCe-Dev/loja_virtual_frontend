59ab249aeac6880bfd578f1020f53ba29eb1c3df
luzG25 <gdluz@uta.cv>
2025-07-21 06:22:10 -0100

    correcoes para deploy4

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "export",
   distDir: "out",
   // TODO: corrigir este config
   //The "images.domains" configuration is deprecated. Please use "images.remotePatterns" configuration instead.
