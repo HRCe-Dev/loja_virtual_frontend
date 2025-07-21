1892fc40d02db10de99b92872602eab59ce98fc1
luzG25 <gdluz@uta.cv>
2025-07-21 05:51:40 -0100

    configuração para deploy em cloudflare pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  //output: 'export',
+  output: "export",
+  distDir: "out",
   // TODO: corrigir este config
   //The "images.domains" configuration is deprecated. Please use "images.remotePatterns" configuration instead.
   images: {
