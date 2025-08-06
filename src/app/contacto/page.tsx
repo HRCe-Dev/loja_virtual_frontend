50db9033efdc43329fb0c1953c3e84bc5aafd484
luzG25 <gdluz@uta.cv>
2025-08-06 10:54:26 -0100

    aprimoracoes de pagina de contactos e sobrenos passar imagens para cloudnary trocar promocoes para superofertas correcao de contactos

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,22 +1,23 @@
 "use client";
+import ChatModal from "@/componentes/ChatModal";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Contacto() {
   return (
     <div className="flex flex-col pb-10 pl-4">
-
       <div className="w-full max-w-2xl font-medium flex flex-col justify-center items-start mx-20 my-10 gap-2 text-center sm:text-left max-sm:text-left max-sm:mx-auto">
         <p className="text-4xl font-bold max-sm:text-2xl">Fala conosco</p>
         <p className="font-light text-2xl">
-          Precisas de ajuda, tens dúvidas ou queres pedir uma proposta? Entra em contacto - respondemos rápido!
+          Precisas de ajuda, tens dúvidas ou queres pedir uma proposta? Entra em
+          contacto - respondemos rápido!
         </p>
       </div>
 
-  
       <div className="w-full flex justify-end my-10 max-sm:my-0">
         <Image
-          src="/img/mockup1.png"
+          src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479212/mockup1_myaxsk.png"
           alt="Mockup 1"
           width={1000}
           height={300}
@@ -24,7 +25,7 @@ export default function Contacto() {
         />
 
         <Image
-          src="/img/mockup2.png"
+          src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479216/mockup2_y0uory.png"
           alt="Mockup 2"
           width={1000}
           height={300}
@@ -32,64 +33,138 @@ export default function Contacto() {
         />
       </div>
 
-
       <div className="font-medium flex flex-col justify-center items-start w-full max-w-md mx-20 my-8 max-sm:text-left max-sm:mx-auto mt-32">
         <p className="text-4xl max-sm:text-3xl">Informações de Contacto</p>
         <div className="flex flex-col gap-4 mt-5">
           <div className="flex gap-4 items-center">
             <div className="w-8">
-              <Image src="/img/phone.png" alt="Telefone" width={100} height={100} className="w-full h-auto object-cover" />
+              <Image
+                src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479216/phone_tzrqzv.png"
+                alt="Telefone"
+                width={100}
+                height={100}
+                className="w-full h-auto object-cover"
+              />
             </div>
-            <a href="tel:+2383478335" className="text-blue-900">(+238) 3478335</a>
+            <p>
+              <a href="tel:+2383478335" className="text-blue-900">
+                (+238) 3478335
+              </a>{" "}
+              |{" "}
+              <a href="tel:+2385931938" className="text-blue-900">
+                (+238) 5931938
+              </a>
+            </p>
           </div>
 
           <div className="flex gap-4 items-center">
             <div className="w-8">
-              <Image src="/img/whatsapp.png" alt="WhatsApp" width={100} height={100} className="w-full h-auto object-cover" />
+              <Image
+                src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479216/whatsapp_yzahe3.png"
+                alt="WhatsApp"
+                width={100}
+                height={100}
+                className="w-full h-auto object-cover"
+              />
             </div>
-            <a href="https://wa.me/2383478335" className="text-blue-900">(+238) 3478335</a>
+            <a href="https://wa.me/2385931938" className="text-blue-900">
+              (+238) 5931938
+            </a>
           </div>
 
           <div className="flex gap-4 items-center">
             <div className="w-8">
-              <Image src="/img/gmail.png" alt="Email" width={600} height={300} className="w-full h-auto object-cover" />
+              <Image
+                src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479210/gmail_h1o79g.png"
+                alt="Email"
+                width={600}
+                height={300}
+                className="w-full h-auto object-cover"
+              />
             </div>
-            <a href="mailto:geral@hrcelda.com" className="text-blue-900">geral@hrcelda.com</a>
+            <a href="mailto:geral@hrcelda.com" className="text-blue-900">
+              geral@hrcelda.com
+            </a>
           </div>
         </div>
       </div>
-
 
       <div className="font-medium flex flex-col justify-center items-start w-full max-w-md mx-20 my-8  max-sm:text-left max-sm:mx-auto">
-        <p className="text-2xl">Ou se preferir, entre em contacto conosco através de nossas redes:</p>
+        <p className="text-2xl">
+          Ou se preferir, entre em contacto conosco através de nossas redes:
+        </p>
         <div className="flex gap-4 mt-5">
-          <div className="w-8">
-            <Image src="/img/facebook.png" alt="Facebook" width={100} height={100} className="w-full h-auto object-cover" />
-          </div>
-          <div className="w-8">
-            <Image src="/img/instagram.png" alt="Instagram" width={100} height={100} className="w-full h-auto object-cover" />
-          </div>
-          <div className="w-8">
-            <Image src="/img/linkedin.png" alt="LinkedIn" width={100} height={100} className="w-full h-auto object-cover" />
-          </div>
+          <Link
+            href="https://www.facebook.com/hrcelda/"
+            className="w-8"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479210/facebook_apfpvs.png"
+              alt="Facebook"
+              width={100}
+              height={100}
+              className="w-full h-auto object-cover"
+            />
+          </Link>
+          <Link
+            href="https://www.instagram.com/hrce.lda/"
+            className="w-8"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479210/instagram_ubeloh.png"
+              alt="Instagram"
+              width={100}
+              height={100}
+              className="w-full h-auto object-cover"
+            />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/company/hrce-lda"
+            className="w-8"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479211/linkedin_wyxf6j.png"
+              alt="LinkedIn"
+              width={100}
+              height={100}
+              className="w-full h-auto object-cover"
+            />
+          </Link>
         </div>
       </div>
 
-
-
-
-
-
-      <div className="flex justify-center items-center mb-14 max-sm:mx-10">
+      <div
+        className="flex justify-center items-center mb-14 max-sm:mx-10"
+        hidden
+      >
         <form className="bg-[#265674] p-6 rounded-lg w-full max-w-2xl grid grid-cols-1 sm:grid-cols-2 gap-4 text-white">
           <div className="flex flex-col">
-            <label htmlFor="nome" className="text-sm font-semibold mb-1">Nome</label>
-            <input type="text" id="nome" placeholder="Seu Nome" className="p-3 rounded text-black bg-white text-sm" />
+            <label htmlFor="nome" className="text-sm font-semibold mb-1">
+              Nome
+            </label>
+            <input
+              type="text"
+              id="nome"
+              placeholder="Seu Nome"
+              className="p-3 rounded text-black bg-white text-sm"
+            />
           </div>
 
           <div className="flex flex-col">
-            <label htmlFor="assunto" className="text-sm font-semibold mb-1">Assunto</label>
-            <select id="assunto" className="p-3 rounded text-black bg-white text-sm" defaultValue="Dúvida">
+            <label htmlFor="assunto" className="text-sm font-semibold mb-1">
+              Assunto
+            </label>
+            <select
+              id="assunto"
+              className="p-3 rounded text-black bg-white text-sm"
+              defaultValue="Dúvida"
+            >
               <option>Dúvida</option>
               <option>Orçamento</option>
               <option>Suporte</option>
@@ -97,32 +172,62 @@ export default function Contacto() {
           </div>
 
           <div className="flex flex-col">
-            <label htmlFor="email" className="text-sm font-semibold mb-1">E-mail</label>
-            <input type="email" id="email" placeholder="Seu E-mail" className="p-3 rounded text-black bg-white text-sm" />
+            <label htmlFor="email" className="text-sm font-semibold mb-1">
+              E-mail
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Seu E-mail"
+              className="p-3 rounded text-black bg-white text-sm"
+            />
           </div>
 
           <div className="flex flex-col">
-            <label htmlFor="telefone" className="text-sm font-semibold mb-1">Telefone</label>
-            <input type="text" id="telefone" placeholder="Seu Telefone" className="p-3 rounded text-black bg-white text-sm" />
+            <label htmlFor="telefone" className="text-sm font-semibold mb-1">
+              Telefone
+            </label>
+            <input
+              type="text"
+              id="telefone"
+              placeholder="Seu Telefone"
+              className="p-3 rounded text-black bg-white text-sm"
+            />
           </div>
 
           <div className="flex flex-col sm:col-span-2">
-            <label htmlFor="mensagem" className="text-sm font-semibold mb-1">Mensagem</label>
-            <textarea id="mensagem" placeholder="Digite sua mensagem" className="p-3 rounded h-28 resize-none text-black bg-white text-sm" />
+            <label htmlFor="mensagem" className="text-sm font-semibold mb-1">
+              Mensagem
+            </label>
+            <textarea
+              id="mensagem"
+              placeholder="Digite sua mensagem"
+              className="p-3 rounded h-28 resize-none text-black bg-white text-sm"
+            />
           </div>
 
           <div className="sm:col-span-2 flex justify-end">
-            <button type="submit" className="bg-orange-500 text-white px-6 py-2 rounded hover:bg-orange-600">
+            <button
+              type="submit"
+              className="bg-orange-500 text-white px-6 py-2 rounded hover:bg-orange-600"
+            >
               Enviar
             </button>
           </div>
         </form>
       </div>
 
-
       <div className="px-4 sm:px-16">
-        <Image src="/img/banner3.jpeg" alt="Banner" width={1000} height={100} className="w-full h-auto object-cover" />
+        <Image
+          src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479210/banner_contacto.jpg"
+          alt="Banner"
+          width={1000}
+          height={100}
+          className="w-full h-auto object-cover"
+        />
       </div>
+
+      <ChatModal />
     </div>
   );
 }
--- src/app/sobrenos/page.tsx
+++ src/app/sobrenos/page.tsx
@@ -1,187 +1,199 @@
 "use client";
 import Image from "next/image";
 
-
 export default function Sobrenos() {
-    return (
-        <div className="w-full">
+  return (
+    <div className="w-full">
+      <div className="flex">
+        <Image
+          src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479210/banner_sobre_nos.jpg"
+          alt="Banner de Sobre Nos"
+          width={600}
+          height={300}
+          className="w-full h-auto object-cover mb-10"
+        />
+      </div>
 
-            <div className="flex">
-                <Image
-                    src="img/banner2.jpeg"
-                    alt="Banner 2"
-                    width={600}
-                    height={300}
-                    className="w-full h-auto object-cover mb-10"
-                />
-            </div>
-
-
-            <div className="flex flex-col sm:flex-row items-center justify-center text-center gap-8 w-full px-4 mb-10">
-                <div className="w-full sm:w-1/5">
-                    <Image
-                        src="img/logo.png"
-                        alt="Logo"
-                        width={600}
-                        height={300}
-                        className="w-full h-auto object-cover"
-                    />
-                </div>
-                <div className="w-full sm:w-1/3">
-                    <p className="text-lg sm:text-2xl font-bold text-[#265674]">
-                        Somos apaixonados por tecnologia e acreditamos que cada cliente merece mais do que apenas um bom produto: merece uma experiência. Esta página conta um pouco da nossa jornada.
-                    </p>
-                </div>
-            </div>
-
-
-            <div className="flex justify-center py-4">
-                <p className="text-3xl font-extrabold text-[#265674]">A História da Marca</p>
-            </div>
-
-            <div className="flex flex-col sm:flex-row items-center justify-center text-center gap-8 w-full px-4 mb-10">
-                <div className="w-full sm:w-1/3 text-left">
-                    <p className="text-xl sm:text-2xl font-bold text-[#265674] pb-4">Tudo começou com um sonho em comum</p>
-                    <p className="text-sm text-left">
-                        A HRCe nasceu da visão e do espírito empreendedor de quatro irmãos unidos por um objetivo em comum – criar soluções inovadoras para impulsionar empresas – transformar a sua experiência e paixão num projeto sólido e ambicioso.
-                    </p>
-                </div>
-
-                <div className="w-full sm:w-1/4">
-                    <Image
-                        src="img/logo.png"
-                        alt="Logo"
-                        width={100}
-                        height={100}
-                        className="w-full h-auto object-cover"
-                    />
-                </div>
-            </div>
-
-
-            <div className="flex flex-col sm:flex-row items-center justify-center text-center gap-10 w-full px-4">
-                <div className="w-full sm:w-1/4">
-                    <Image
-                        src="img/logo3.jpeg"
-                        alt="Logo"
-                        width={100}
-                        height={100}
-                        className="w-full h-auto object-cover"
-                    />
-                </div>
-
-                <div className="w-full sm:w-1/3 text-left">
-                    <p className="text-xl sm:text-2xl font-bold text-[#265674] pb-4">Mais do que uma empresa: uma família com propósito</p>
-                    <p className="text-sm text-left">
-                        Desde o primeiro dia, a nossa força vem da nossa equipa. Valorizamos o talento, a dedicação e o compromisso. Juntos, enfrentamos desafios, crescemos com os nossos clientes e mantemos a essência familiar e colaborativa que nos diferencia.
-                    </p>
-                </div>
-            </div>
-
-
-            <div className="flex flex-col items-center justify-center text-center w-full py-10">
-                <div className="w-full sm:w-1/3">
-                    <p className="text-2xl sm:text-3xl font-bold text-[#265674]">O que fazemos com excelência</p>
-                </div>
-
-                <div className="w-full">
-                    <Image
-                        src="img/banner4.png"
-                        alt="Banner"
-                        width={500}
-                        height={100}
-                        className="w-full h-auto object-cover"
-                    />
-                </div>
-            </div>
-
-
-            <div className="flex flex-col sm:flex-row items-center justify-center text-center gap-8 w-full pb-20 px-4">
-                <div className="w-full sm:w-1/3 text-left">
-                    <p className="text-xl sm:text-2xl font-bold text-[#265674] pb-4">Crescimento com confiança</p>
-                    <p className="text-sm text-left">
-                        A nossa jornada é feita de conquistas, parcerias duradouras e crescimento sustentado. Evoluímos sem nunca esquecer os princípios que nos trouxeram até aqui. Cada cliente é parte dessa história. Cada projeto é um novo passo em frente.
-                    </p>
-                </div>
-
-                <div className="w-full sm:w-1/4">
-                    <Image
-                        src="img/logo3.jpeg"
-                        alt="Logo"
-                        width={100}
-                        height={100}
-                        className="w-full h-auto object-cover"
-                    />
-                </div>
-            </div>
-
-            <div className="flex flex-col sm:flex-row items-center justify-center text-center gap-8 w-full pb-20 px-4">
-                <div className="w-full sm:w-1/4">
-                    <Image
-                        src="img/logo3.jpeg"
-                        alt="Logo"
-                        width={100}
-                        height={100}
-                        className="w-full h-auto object-cover"
-                    />
-                </div>
-
-                <div className="w-full sm:w-1/3 text-left">
-                    <p className="text-xl sm:text-2xl font-bold text-[#265674] pb-4">O nosso futuro começa agora</p>
-                    <p className="text-sm text-left">
-                        O caminho continua e a nossa missão é clara: ser um parceiro estratégico para negócios que querem crescer com confiança e inovação. Estamos prontos para construir os próximos capítulos com quem acredita no valor da dedicação.
-                    </p>
-                </div>
-            </div>
-
-
-            <div className="flex flex-col items-center justify-center text-center w-full py-10">
-
-                <div className="w-full sm:w-1/3">
-                    <p className="text-2xl sm:text-3xl font-extrabold text-[#265674] pb-4">Nosso time</p>
-                </div>
-            </div>
-
-            <div className="w-full px-10 flex flex-wrap ">
-
-                <Image
-                    src="https://hrcelda.com/wp-content/uploads/2024/03/cropped-team1.png"
-                    alt="Logo"
-                    width={100}
-                    height={100}
-                    className="sm:w-1/4 border-2 border-gray-200"
-                />
-
-                <Image
-                    src="https://hrcelda.com/wp-content/uploads/2024/03/cropped-team2.png"
-                    alt="Logo"
-                    width={100}
-                    height={100}
-                    className="w-1/2 sm:w-1/4 border-2 border-gray-200"
-                />
-
-                <Image
-                    src="https://hrcelda.com/wp-content/uploads/2024/03/cropped-team3.png"
-                    alt="Logo"
-                    width={100}
-                    height={100}
-                    className="w-1/2 sm:w-1/4 border-2 border-gray-200"
-                />
-
-
-
-                <Image
-                    src="https://hrcelda.com/wp-content/uploads/2024/03/cropped-team4.png"
-                    alt="Logo"
-                    width={100}
-                    height={100}
-                    className="w-1/2 sm:w-1/4 border-2 border-gray-200"
-                />
-
-
-            </div>
-
-
+      <div className="flex flex-col sm:flex-row items-center justify-center text-center gap-8 w-full px-4 mb-10">
+        <div className="w-full sm:w-1/5">
+          <Image
+            src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479211/hrce_logo_mascote.png"
+            alt="Logo"
+            width={600}
+            height={300}
+            className="w-full h-auto object-cover"
+          />
         </div>
-    );
+        <div className="w-full sm:w-1/3">
+          <p className="text-lg sm:text-2xl font-bold text-[#265674]">
+            Somos apaixonados por tecnologia e acreditamos que cada cliente
+            merece mais do que apenas um bom produto: merece uma experiência.
+            Esta página conta um pouco da nossa jornada.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-center py-4">
+        <p className="text-3xl font-extrabold text-[#265674]">
+          A História da Marca
+        </p>
+      </div>
+
+      <div className="flex flex-col sm:flex-row items-center justify-center text-center gap-8 w-full px-4 mb-10">
+        <div className="w-full sm:w-1/3 text-left">
+          <p className="text-xl sm:text-2xl font-bold text-[#265674] pb-4">
+            Tudo começou com um sonho em comum
+          </p>
+          <p className="text-sm text-left">
+            A HRCe nasceu da visão e do espírito empreendedor de quatro irmãos
+            unidos por um objetivo em comum – criar soluções inovadoras para
+            impulsionar empresas – transformar a sua experiência e paixão num
+            projeto sólido e ambicioso.
+          </p>
+        </div>
+
+        <div className="w-full sm:w-1/4">
+          <Image
+            src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479211/hrce_logo_mascote.png"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="w-full h-auto object-cover"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col sm:flex-row items-center justify-center text-center gap-10 w-full px-4">
+        <div className="w-full sm:w-1/4">
+          <Image
+            src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479211/logo3_xujjju.jpg"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="w-full h-auto object-cover"
+          />
+        </div>
+
+        <div className="w-full sm:w-1/3 text-left">
+          <p className="text-xl sm:text-2xl font-bold text-[#265674] pb-4">
+            Mais do que uma empresa: uma família com propósito
+          </p>
+          <p className="text-sm text-left">
+            Desde o primeiro dia, a nossa força vem da nossa equipa. Valorizamos
+            o talento, a dedicação e o compromisso. Juntos, enfrentamos
+            desafios, crescemos com os nossos clientes e mantemos a essência
+            familiar e colaborativa que nos diferencia.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center text-center w-full py-10">
+        <div className="w-full sm:w-1/3">
+          <p className="text-2xl sm:text-3xl font-bold text-[#265674]">
+            O que fazemos com excelência
+          </p>
+        </div>
+
+        <div className="w-full">
+          <Image
+            src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479210/carrosel_hrce_servicos.png"
+            alt="Banner"
+            width={500}
+            height={100}
+            className="w-full h-auto object-cover"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col sm:flex-row items-center justify-center text-center gap-8 w-full pb-20 px-4">
+        <div className="w-full sm:w-1/3 text-left">
+          <p className="text-xl sm:text-2xl font-bold text-[#265674] pb-4">
+            Crescimento com confiança
+          </p>
+          <p className="text-sm text-left">
+            A nossa jornada é feita de conquistas, parcerias duradouras e
+            crescimento sustentado. Evoluímos sem nunca esquecer os princípios
+            que nos trouxeram até aqui. Cada cliente é parte dessa história.
+            Cada projeto é um novo passo em frente.
+          </p>
+        </div>
+
+        <div className="w-full sm:w-1/4">
+          <Image
+            src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479211/logo3_xujjju.jpg"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="w-full h-auto object-cover"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col sm:flex-row items-center justify-center text-center gap-8 w-full pb-20 px-4">
+        <div className="w-full sm:w-1/4">
+          <Image
+            src="https://res.cloudinary.com/dmk688hlh/image/upload/v1754479211/logo3_xujjju.jpg"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="w-full h-auto object-cover"
+          />
+        </div>
+
+        <div className="w-full sm:w-1/3 text-left">
+          <p className="text-xl sm:text-2xl font-bold text-[#265674] pb-4">
+            O nosso futuro começa agora
+          </p>
+          <p className="text-sm text-left">
+            O caminho continua e a nossa missão é clara: ser um parceiro
+            estratégico para negócios que querem crescer com confiança e
+            inovação. Estamos prontos para construir os próximos capítulos com
+            quem acredita no valor da dedicação.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center text-center w-full py-10">
+        <div className="w-full sm:w-1/3">
+          <p className="text-2xl sm:text-3xl font-extrabold text-[#265674] pb-4">
+            Nosso time
+          </p>
+        </div>
+      </div>
+
+      <div className="w-full px-10 flex flex-wrap ">
+        <Image
+          src="https://hrcelda.com/wp-content/uploads/2024/03/cropped-team1.png"
+          alt="Logo"
+          width={100}
+          height={100}
+          className="sm:w-1/4 border-2 border-gray-200"
+        />
+
+        <Image
+          src="https://hrcelda.com/wp-content/uploads/2024/03/cropped-team2.png"
+          alt="Logo"
+          width={100}
+          height={100}
+          className="w-1/2 sm:w-1/4 border-2 border-gray-200"
+        />
+
+        <Image
+          src="https://hrcelda.com/wp-content/uploads/2024/03/cropped-team3.png"
+          alt="Logo"
+          width={100}
+          height={100}
+          className="w-1/2 sm:w-1/4 border-2 border-gray-200"
+        />
+
+        <Image
+          src="https://hrcelda.com/wp-content/uploads/2024/03/cropped-team4.png"
+          alt="Logo"
+          width={100}
+          height={100}
+          className="w-1/2 sm:w-1/4 border-2 border-gray-200"
+        />
+      </div>
+    </div>
+  );
 }
