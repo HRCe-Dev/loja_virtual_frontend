8e0d7a3f36b87b9270cc22e66229217bc7cc83ac
luzG25 <gdluz@uta.cv>
2025-08-13 13:23:43 -0100

    msg de erro em pagamento aprimorado para mais visibilidade

--- src/componentes/ErroMiniModal.tsx
+++ src/componentes/ErroMiniModal.tsx
@@ -21,10 +21,19 @@ export default function ErroMiniModal({ error }: ErroMiniModalProps) {
   if (!visivel || !error) return null;
 
   return (
-    <div className="fixed bottom-10 right-4 z-[51]">
-      <div className="flex items-center gap-2 max-w-xs p-4 bg-red-100 border border-red-400 text-red-700 rounded-lg shadow-lg animate-fade-in-down">
-        <XCircle className="mt-0.5 text-red-500" size={20} />
-        <span className="text-sm">{error}</span>
+    <div className="fixed inset-0 flex items-center justify-center z-[51]">
+      <div className="relative flex flex-col items-center gap-2 max-w-xs p-4 bg-red-100 border border-red-400 text-red-500 rounded-lg shadow-lg animate-fade-in-down">
+        <XCircle className="font-bold mt-0.5 text-red-500" size={30} />
+        <span className="text-xl font-bold text-center">{error}</span>
+
+        <button
+          onClick={() => {
+            setVisivel(false);
+          }}
+          className="hover:text-white text-sm hover:bg-gray-500 px-5 py-1 rounded-2xl border border-gray-500 mt-2"
+        >
+          OK
+        </button>
       </div>
     </div>
   );
